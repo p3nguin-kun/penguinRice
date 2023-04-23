aad52309384ebf1fe5ed7a0f7f4bca706732e7c9
Khanh Hien Hoang <kh4nhhi3n@hotmail.com>
2023-04-23 15:53:21 +0700

    Update i3-install.sh

--- i3-install.sh
+++ i3-install.sh
@@ -3,12 +3,12 @@ git clone https://github.com/p3nguin-kun/cozy-gruvbox-i3
 cd cozy-gruvbox-i3
 cp -R .config ~/                                            
 chmod -R +x ~/.config/i3/scripts
-sed -i 's|\(exec --no-startup-id ~/set_once.sh\)|# \1|' ~/.config/i3/config
 curl -LO https://raw.githubusercontent.com/p3nguin-kun/pasaai/main/i3-packages-list
-sudo pacman -S --needed - < i3
+sudo pacman -S --needed - < i3-packages-list
 echo /usr/local/bin/fish | sudo tee -a /etc/shells
 chsh -s /usr/bin/fish
 grep "^Color" /etc/pacman.conf >/dev/null || sed -i "s/^#Color$/Color/" /etc/pacman.conf
 grep "ILoveCandy" /etc/pacman.conf >/dev/null || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
 cd ..
 rm -rf cozy-minimal-i3
+clear
