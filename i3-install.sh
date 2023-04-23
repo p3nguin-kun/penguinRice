4f8ebf1a1778d5ab2058b181013cfb2cfa16f5ec
Khanh Hien Hoang <kh4nhhi3n@hotmail.com>
2023-04-23 15:53:32 +0700

    Update i3-install.sh

--- i3-install.sh
+++ i3-install.sh
@@ -6,7 +6,7 @@ chmod -R +x ~/.config/i3/scripts
 curl -LO https://raw.githubusercontent.com/p3nguin-kun/pasaai/main/i3-packages-list
 sudo pacman -S --needed - < i3-packages-list
 echo /usr/local/bin/fish | sudo tee -a /etc/shells
-chsh -s /usr/bin/fish
+chsh -s /usr/local/bin/fish
 grep "^Color" /etc/pacman.conf >/dev/null || sed -i "s/^#Color$/Color/" /etc/pacman.conf
 grep "ILoveCandy" /etc/pacman.conf >/dev/null || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
 cd ..
