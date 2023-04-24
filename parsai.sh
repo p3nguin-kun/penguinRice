5b9e3c6038cdecafa2523699f1a8d9745297f6a0
Khanh Hien Hoang <kh4nhhi3n@hotmail.com>
2023-04-24 16:17:23 +0700

    Update parsai.sh

--- parsai.sh
+++ parsai.sh
@@ -36,6 +36,8 @@ chsh -s /usr/local/bin/fish
 grep "^Color" /etc/pacman.conf >/dev/null || sed -i "s/^#Color$/Color/" /etc/pacman.conf
 grep "ILoveCandy" /etc/pacman.conf >/dev/null || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
 # Start LightDM with OS
+sudo systemctl enable lightdm
+sudo systemctl enable lightdm.service
 # Delete dotfiles repo
 cd ..
 rm -rf cozy-minimal-i3
