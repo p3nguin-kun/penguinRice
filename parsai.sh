01c1929153fb151e6b14449e816acdc893a1f743
Khanh Hien Hoang <kh4nhhi3n@hotmail.com>
2023-04-28 18:30:06 +0700

    Update parsai.sh

--- parsai.sh
+++ parsai.sh
@@ -23,7 +23,7 @@ cd ~/
 git clone https://github.com/p3nguin-kun/cozy-gruvbox-i3
 
 # Install packages
-sudo pacman -S --noconfirm acpi arandr archlinux-xdg-menu awesome-terminal-fonts dex dmenu dunst feh file-roller firefox fish galculator gthumb gtk-engine-murrine gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb htop i3-wm i3blocks i3lock i3status jq libreoffice-still lightdm lightdm-gtk-greeter lxappearance-gtk3 mpc mpd mpv ncmpcpp neofetch networkmanager network-manager-applet numlockx pavucontrol playerctl polkit-gnome rofi scrot sed sysstat thunar thunar-archive-plugin thunar-volman ttc-iosevka ttf-font-awesome tumbler unrar unzip vim xbindkeys xdg-user-dirs-gtk xfce4-terminal xorg-xbacklight xorg-xdpyinfo zathura zip
+sudo pacman -S --noconfirm acpi alsa-utils arandr archlinux-xdg-menu awesome-terminal-fonts dex dmenu dunst feh file-roller firefox fish galculator gthumb gtk-engine-murrine gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb htop i3-wm i3blocks i3lock i3status jq libreoffice-still lightdm lightdm-gtk-greeter lxappearance-gtk3 mpc mpd mpv ncmpcpp neofetch networkmanager network-manager-applet numlockx pavucontrol playerctl polkit-gnome rofi scrot sed sysstat thunar thunar-archive-plugin thunar-volman ttc-iosevka ttf-font-awesome tumbler unrar unzip vim xbindkeys xdg-user-dirs-gtk xfce4-terminal xorg-xbacklight xorg-xdpyinfo zathura zip
 
 # Install yay (an aur helper)
 sudo pacman -S --needed base-devel
@@ -51,8 +51,9 @@ sudo sed -i "s/#greeter-session=example-gtk-gnome/greeter-session=lightdm-gtk-gr
 sudo sed -i "s/#background=/background=\/usr\/share\/backgrounds\/wall2.png/g" /etc/lightdm/lightdm-gtk-greeter.conf
 sudo sed -i "s/#font-name/font-name=Iosevka/g" /etc/lightdm/lightdm-gtk-greeter.conf
 
-# Install Qogir-icon-theme
+# Install Icons and Themes
 yay -S qogir-icon-theme
+yay -S gruvbox-dark-gtk
 
 # Config pacman (for what?)
 grep "^Color" /etc/pacman.conf >/dev/null || sudo sed -i "s/^#Color$/Color/" /etc/pacman.conf
