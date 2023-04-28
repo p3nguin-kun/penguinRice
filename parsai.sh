a93ef1c85071b78ea394cc77c3c419e76e92d0fb
Khanh Hien Hoang <kh4nhhi3n@hotmail.com>
2023-04-28 09:58:20 +0000

    Add files via upload

--- parsai.sh
+++ parsai.sh
@@ -23,7 +23,7 @@ cd ~/
 git clone https://github.com/p3nguin-kun/cozy-gruvbox-i3
 
 # Install packages
-sudo pacman -S --noconfirm acpi arandr archlinux-xdg-menu awesome-terminal-fonts dex dmenu dunst feh file-roller firefox fish galculator gthumb gtk-engine-murrine gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb htop i3-wm i3blocks i3lock i3status jq libreoffice-still lightdm lightdm-gtk-greeter lightdm-slick-greeter lxappearance-gtk3 mpc mpd mpv ncmpcpp neofetch networkmanager network-manager-applet numlockx playerctl polkit-gnome rofi scrot sed sysstat thunar thunar-archive-plugin thunar-volman ttc-iosevka ttf-font-awesome tumbler unrar unzip vim xbindkeys xdg-user-dirs-gtk xfce4-terminal xorg-xbacklight xorg-xdpyinfo zathura zip
+sudo pacman -S --noconfirm acpi arandr archlinux-xdg-menu awesome-terminal-fonts dex dmenu dunst feh file-roller firefox fish galculator gthumb gtk-engine-murrine gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb htop i3-wm i3blocks i3lock i3status jq libreoffice-still lightdm lightdm-gtk-greeter lightdm-slick-greeter lxappearance-gtk3 mpc mpd mpv ncmpcpp neofetch networkmanager network-manager-applet numlockx pavucontrol playerctl polkit-gnome rofi scrot sed sysstat thunar thunar-archive-plugin thunar-volman ttc-iosevka ttf-font-awesome tumbler unrar unzip vim xbindkeys xdg-user-dirs-gtk xfce4-terminal xorg-xbacklight xorg-xdpyinfo zathura zip
 
 # Install yay (an aur helper)
 sudo pacman -S --needed base-devel
@@ -45,7 +45,8 @@ chsh -s /usr/bin/fish
 
 #Config LightDM
 echo "exec i3" >> ~/.xinitrc
-sudo sed -i "s/#greeter-session=example-gtk-gnome/greeter-session=lightdm-slick-greeter/g" /etc/lightdm/lightdm.conf
+sudo sed -i "s/#greeter-session=example-gtk-gnome/greeter-session=lightdm-gtk-greeter/g" /etc/lightdm/lightdm.conf
+sudo sed -i "s/#background=/background=~\/.config\/wallpapers\/wall2.png/g" /etc/lightdm/lightdm-gtk-greeter.conf
 
 # Install Qogir-icon-theme
 yay -S qogir-icon-theme
@@ -58,9 +59,10 @@ grep "ILoveCandy" /etc/pacman.conf >/dev/null || sudo sed -i "/#VerbosePkgLists/
 sudo systemctl enable lightdm
 
 # Delete dotfiles repo
-cd ..
+cd ~/
 rm -rf cozy-minimal-i3
 rm -rf Qogir-icon-theme
+rm -rf yay
 
 # Finish ricing
 clear
