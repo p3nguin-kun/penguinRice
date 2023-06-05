62d12f82ac64a10523b5d5c6c08171610820f288
Khanh Hien Hoang <kh4nhhi3n@hotmail.com>
2023-06-05 17:12:33 +0000

    Update penguinrice-i3wm.sh

--- penguinrice-i3wm.sh
+++ penguinrice-i3wm.sh
@@ -79,7 +79,7 @@ else
 fi
 
 echo "Installing AUR packages"
-yay -S --noconfirm betterlockscreen dragon-drop gruvbox-dark-gtk picom-pijulius-git pixterm-git qogir-icon-theme sddm-theme-aerial-git ttf-icomoon-feather
+yay -S --noconfirm betterlockscreen dragon-drop everforest-gtk-theme-git picom-pijulius-git pixterm-git qogir-icon-theme sddm-theme-aerial-git ttf-icomoon-feather
 
 # Preparing folders
 logo "Preparing folders"
