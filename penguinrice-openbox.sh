f3b4e5a756ec64ba8aa49a6627d271c546f28f8b
Khanh Hien Hoang <kh4nhhi3n@hotmail.com>
2023-06-08 10:37:07 +0000

    Update penguinrice-openbox.sh

--- penguinrice-openbox.sh
+++ penguinrice-openbox.sh
@@ -78,7 +78,7 @@ else
 fi
 
 echo "Installing AUR packages"
-yay -S --noconfirm betterlockscreen dragon-drop everforest-gtk-theme-git pixterm-git qogir-icon-theme sddm-theme-aerial-git ttf-icomoon-feather
+yay -S --noconfirm betterlockscreen dragon-drop everforest-gtk-theme-git pixterm-git qogir-icon-theme sddm-theme-corners-git ttf-icomoon-feather
 
 # Preparing folders
 logo "Preparing folders"
