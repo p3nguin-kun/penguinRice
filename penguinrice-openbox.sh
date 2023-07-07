5913e924fb45038d677979279af495d3f08468cd
p3nguin-kun <p3nguinkun@proton.me>
2023-07-07 04:57:57 +0000

    Update penguinrice-openbox.sh

--- penguinrice-openbox.sh
+++ penguinrice-openbox.sh
@@ -87,7 +87,7 @@ is_installed() {
 printf "%s%sChecking for required packages%s\n" "${BLD}" "${CBL}" "${CNC}"
 for paquete in "${dependencies[@]}"; do
 	if ! is_installed "$paquete"; then
-		yay -S "$paquete"
+		yay -S --noconfirm "$paquete"
 		printf "\n"
 	else
 		printf '%s%s is already installed on your system!%s\n' "${CGR}" "$paquete" "${CNC}"
