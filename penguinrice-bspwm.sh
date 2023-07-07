fdd5cb0ec82dcf0f348cc657827d3c64157877db
p3nguin-kun <p3nguinkun@proton.me>
2023-07-07 07:52:25 +0000

    Update penguinrice-bspwm.sh

--- penguinrice-bspwm.sh
+++ penguinrice-bspwm.sh
@@ -77,7 +77,7 @@ clear
 # Install packages
 logo "Installing needed packages"
 
-dependencies=(alacritty arandr betterlockscreen bspwm btop calcurse dragon-drop dunst feh fish git gtk-engine-murrine inetutils jq librewolf-bin lightdm lightdm-webkit2-greeter lmaofetch lxappearance-gtk3 mpc mpd mpv ncmpcpp neovim networkmanager network-manager-applet pamixer pavucontrol picom-pijulius-git polkit-gnome polybar qogir-icon-theme ranger rofi sed sxhkd thunar thunar-archive-plugin thunar-volman ttf-icomoon-feather ttf-iosevka-nerd ttf-sarasa-gothic udisks2 ueberzug unrar unzip xarchiver xbindkeys xclip xdg-user-dirs-gtk xfce4-power-manager xfce4-screenshooter xss-lock zathura zathura-pdf-mupdf zip)
+dependencies=(alacritty arandr betterlockscreen bspwm btop calcurse dragon-drop dunst feh fish git gtk-engine-murrine gvfs inetutils jq librewolf-bin lightdm lightdm-webkit2-greeter lmaofetch lxappearance-gtk3 mpc mpd mpv ncmpcpp neovim networkmanager network-manager-applet pamixer pavucontrol picom-pijulius-git polkit-gnome polybar qogir-icon-theme ranger rofi sed sxhkd thunar thunar-archive-plugin thunar-volman ttf-icomoon-feather ttf-iosevka-nerd ttf-sarasa-gothic udisks2 ueberzug unrar unzip xarchiver xbindkeys xclip xdg-user-dirs-gtk xfce4-power-manager xfce4-screenshooter xss-lock zathura zathura-pdf-mupdf zip)
 
 is_installed() {
 	pacman -Qi "$1" &>/dev/null
